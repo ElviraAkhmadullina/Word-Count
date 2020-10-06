25b8e6b4f5dd223448bb0c37f27be559de9e3476
Elvira Akhmadullina <ellyakhmadullina@gmail.com>
2020-10-06 07:26:13 -0700

    Add argv

--- wordcount.py
+++ wordcount.py
@@ -1,1 +1,21 @@
-# put your code here.
+import sys
+
+def file_word_count(filename):
+    word_counts = {}
+
+    text_file = open(filename)
+
+    for line in text_file:
+        for word in line.split():
+            word_counts[word] = word_counts.get(word, 0) + 1
+
+    for key, value in word_counts.items():
+        print(f"{key} {value}")    
+    
+    return word_counts
+
+filename = sys.argv[1]
+file_word_count(filename)
+
+
+
